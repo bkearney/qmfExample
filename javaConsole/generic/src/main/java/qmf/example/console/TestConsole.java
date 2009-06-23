a110e081040789ac12fa3f41f33ce0ae47b9e6eb
Bryan Kearney <bkearney@redhat.com>
2009-06-23 14:06:55 -0400

    Added the java console to build off of the latest qpid code

---
+++ javaConsole/generic/src/main/java/qmf/example/console/TestConsole.java
@@ -0,0 +1,57 @@
+package qmf.example.console ;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.qpid.console.MethodResult;
+import org.apache.qpid.console.QMFObject;
+import org.apache.qpid.console.Session;
+import org.apache.qpid.console.XMLUtil;
+
+public class TestConsole
+{
+    public static void main(String args[]) throws Exception
+    {
+        Session session = new Session();
+        session.addBroker("amqp://guest:guest@/?brokerlist='tcp://localhost'");
+        HashMap<String, Object> qParams = new HashMap<String, Object>();
+        qParams.put("_class", "ExampleService");
+        qParams.put("_package", "qmf.example.ejb");
+        ArrayList<QMFObject> objs = session.getObjects(qParams);
+        QMFObject service = objs.get(0);
+        MethodResult result = service.invokeMethod("getBase", "Nathan",
+                "A Cool Dude");
+        QMFObject baseObject = (QMFObject) result.getReturnValue("result");
+        System.out.println(String.format("Base: %s", baseObject));
+        System.out.println(String.format("Base.name: %s", baseObject
+                .GetProperty("name")));
+        System.out.println(String.format("Base.description: %s", baseObject
+                .GetProperty("description")));
+        System.out.println(String.format("Base.stuff: %s", baseObject
+                .GetProperty("stuff")));
+        result = service.invokeMethod("getDerived", "Gillian",
+                "A Cool Dudette", 99);
+        QMFObject derivedObject = (QMFObject) result.getReturnValue("result");
+        System.out.println(String.format("Derived: %s", derivedObject));
+        System.out.println(String.format("Derived.name: %s", derivedObject
+                .GetProperty("name")));
+        System.out.println(String.format("Derived.description: %s",
+                derivedObject.GetProperty("description")));
+        System.out.println(String.format("Derived.stuff: %s", derivedObject
+                .GetProperty("stuff")));
+        System.out.println(String.format("Derived.count: %s", derivedObject
+                .GetProperty("count")));
+        result = service.invokeMethod("findMany");
+        List<Object> many = (List<Object>) result.getReturnValue("result");
+        System.out.println(String.format("Many: %s", many));
+        System.out.println(String.format("Many.stuff: %s", ((QMFObject) many
+                .get(0)).GetProperty("stuff")));
+        
+         String[] packages = {"qmf.example", "qmf.example.ejb"} ;
+         System.out.println(XMLUtil.schemaXML(session, packages)) ;
+       
+        Thread.sleep(1000);
+        session.close();
+    }
+}
